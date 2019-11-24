37273108fa82c1a5fd0c4f115518d200c8776c90
hypersaiph <geovanimendoza.h@gmail.com>
2019-11-24 15:08:16 -0400

    ios files

---
+++ Dumb App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dumb App
+//
+//  Created by Geovani Mendoza on 11/24/19.
+//  Copyright © 2019 Geovani Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
